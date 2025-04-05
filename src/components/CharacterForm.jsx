6eb97993f0a19f517797af5e0f8e1927c3dceff4
Lyla Lynn <lyladlynn@gmail.com>
2025-04-05 18:22:22 -0500

    added new components and updated CharacterForm.jsx

--- src/components/CharacterForm.jsx
+++ src/components/CharacterForm.jsx
@@ -5,32 +5,98 @@ import AbilityScoreInput from './components/AbilityScoreInput.jsx';
 import CharacterSheet from './components/CharacterSheet.jsx'
 
 function CharacterForm() {
-    const [character, setCharacter] = useState({
-        name: "",
-        alignment: "",
-        race: "",
-        charClass: "",
-        abilityScores: {
-          strength: 0,
-          dexterity: 0,
-          constitution: 0,
-          intelligence: 0,
-          wisdom: 0,
-          charisma: 0
-        }
-      });
+  const [step, setStep] = useState(1);
+  const [character, setCharacter] = useState({
+    // Character Details:
+      name: '',
+      age: '',
+      alignment: '',
+      appearance: '',
+      faction: '',
+      deity: '',
+    
+    // Build Character:
+      race: '',
+      charClass: '',
+      abilityScores: {
+        strength: 0,
+        dexterity: 0,
+        constitution: 0,
+        intelligence: 0,
+        wisdom: 0,
+        charisma: 0
+      },
+
+    // Backgroud:
+      hometown: '',
+      backstory: '',
+      motivation: '',
+
+    // Spells & Equiptment:
+      spells: [],
+      equiptment: [],
+    });
+
+  const nextStep = () => setStep((prev) => prev + 1);
+  const prevStep = () => setStep((prev) => prev - 1);
 
   return (
     <div>
-        <form>
-        Name & Alignment
-        Race
-        Class
-        Assign Ability Scores
-        Final Character Sheet/Review
-        </form>
+        <h2>
+        Step {step}
+        </h2>
+        {step === 1 && (
+          <CharacterDetails
+            data={character}
+            updateData={setCharacter}
+            next={nextStep}
+          />
+        )}
+
+        {step === 2 && (
+          <BuildCharacter 
+            data={character}
+            updateData={setCharacter}
+            next={nextStep}
+            back={prevStep}
+          /> 
+        )}
+
+        {step === 3 && (
+          <BackgroundSelect 
+            data={character}
+            updateData={setCharacter}
+            next={nextStep}
+            back={prevStep}
+          />
+        )}
+
+        {step === 4 && (
+          <SpellsAndEquiptment
+            data={character}
+            updateData={setCharacter}
+            next={nextStep}
+            back={prevStep}
+          />
+        )}
+
+        {step === 5 && (
+          <CharacterSheet 
+            data={character}
+            back={prevStep}
+          />
+        )}
+
     </div>
+
   );
 };
 
 export default CharacterForm;
+
+// Name & Alignment
+//         Race
+//         Class
+//         Assign Ability Scores
+//         Spells & Gear
+//         Final Character Sheet/Review
