f007d9486c4b507786c9d20509ff74637fa38137
Lyla Lynn <lyladlynn@gmail.com>
2025-05-19 23:00:59 -0500

    changed colors in theme

--- src/components/SpellsAndEquipment.jsx
+++ src/components/SpellsAndEquipment.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import '../spellsAndEquipment.css';
 
 const SpellsAndEquipment = ({ data, updateData, next, back }) => {
